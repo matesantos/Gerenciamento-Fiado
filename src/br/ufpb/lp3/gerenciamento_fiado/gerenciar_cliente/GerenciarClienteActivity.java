1969b1c00ce221f680ee4548997b09a3a97ceaa8
Mateus <matesantos@hotmail.com>
2013-07-03 12:03:40 -0300

    arquivos da tela para cadastrar cliente

--- src/br/ufpb/lp3/gerenciamento_fiado/gerenciar_cliente/GerenciarClienteActivity.java
+++ src/br/ufpb/lp3/gerenciamento_fiado/gerenciar_cliente/GerenciarClienteActivity.java
@@ -4,7 +4,9 @@ package br.ufpb.lp3.gerenciamento_fiado.gerenciar_cliente;
 import br.ufpb.lp3.gerenciamento_fiado.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class GerenciarClienteActivity extends Activity {
 
@@ -20,5 +22,15 @@ public class GerenciarClienteActivity extends Activity {
 		getMenuInflater().inflate(R.menu.gerenciar_cliente, menu);
 		return true;
 	}
+	
+	public void casdastrarCLiente (View v){
+		goToActivity(CadastrarClienteActivity.class);
+	}
+	
+	//método que abrirá outras Activitys
+    private void goToActivity (Class<? extends Activity> activityClass2){
+    	Intent newIntent = new Intent(this,activityClass2);
+    	startActivity(newIntent);
+    }
 
 }
