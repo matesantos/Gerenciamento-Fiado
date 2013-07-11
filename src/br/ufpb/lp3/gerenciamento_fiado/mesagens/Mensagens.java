ef60ba095c1135a196082e9b9287b40e1a0fa77c
Mateus <matesantos@hotmail.com>
2013-07-11 18:13:53 -0300

    tela de login concluida, agora s falta botar a logica do do sistema

---
+++ src/br/ufpb/lp3/gerenciamento_fiado/mesagens/Mensagens.java
@@ -0,0 +1,7 @@
+package br.ufpb.lp3.gerenciamento_fiado.mesagens;
+
+public interface Mensagens {
+	
+	public final static String campoLoginSenhaVazio = "Os campo login e senha não podem estarem vazios!";
+
+}
